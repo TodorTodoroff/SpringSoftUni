b5406050f9e86fdfd3cddeb8ef1797742ab9a2b2
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-06-18 19:07:26 +0300

    started work on AuthController and DTOs

---
+++ SpringFundamentals/BattleShips/src/main/java/com/example/battleships/model/dto/UserRegisterDTO.java
@@ -0,0 +1,64 @@
+package com.example.battleships.model.dto;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+public class UserRegisterDTO {
+
+    @Size(min = 3, max = 10)
+    private String username;
+
+    @Size(min = 5, max = 20)
+    private String fullname;
+
+    @Email
+    private String email;
+
+    @Size(min = 3)
+    private String password;
+
+    @Size(min = 3)
+    private String confirmPassword;
+
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- SpringFundamentals/BattleShips/src/main/java/com/example/battleships/web/AuthController.java
+++ SpringFundamentals/BattleShips/src/main/java/com/example/battleships/web/AuthController.java
@@ -1,7 +1,15 @@
 package com.example.battleships.web;
 
+import com.example.battleships.model.dto.UserRegisterDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthController {
@@ -12,5 +20,31 @@ public class AuthController {
         return "login";
     }
 
+    @ModelAttribute("userRegisterDto")
+    public UserRegisterDTO initialForm() {
+        return new UserRegisterDTO();
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid UserRegisterDTO userRegisterDto,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("userRegisterDto", userRegisterDto);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegisterDto", bindingResult);
+
+        return "redirect:/register";
+        }
+
+
+
+        return "redirect:/login";
+    }
 
 }
