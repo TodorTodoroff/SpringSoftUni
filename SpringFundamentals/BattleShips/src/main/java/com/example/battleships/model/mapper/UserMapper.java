831796e0527319cb433788e608863b04835a7a5f
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-06-19 20:37:40 +0300

    more work on the auth service

--- SpringFundamentals/BattleShips/src/main/java/com/example/battleships/model/mapper/UserMapper.java
+++ SpringFundamentals/BattleShips/src/main/java/com/example/battleships/model/mapper/UserMapper.java
@@ -1,5 +1,6 @@
-package com.example.battleships.model;
+package com.example.battleships.model.mapper;
 
+import com.example.battleships.model.User;
 import com.example.battleships.model.dto.UserRegisterDTO;
 import org.mapstruct.Mapper;
 
--- SpringFundamentals/BattleShips/src/main/java/com/example/battleships/services/AuthService.java
+++ SpringFundamentals/BattleShips/src/main/java/com/example/battleships/services/AuthService.java
@@ -2,6 +2,7 @@ package com.example.battleships.services;
 
 import com.example.battleships.model.User;
 import com.example.battleships.model.dto.UserRegisterDTO;
+import com.example.battleships.model.mapper.UserMapper;
 import com.example.battleships.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,26 @@ import java.util.Optional;
 public class AuthService {
 
     private final UserRepository userRepository;
-
+    private final UserMapper userMapper;
 
     @Autowired
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
 
+        this.userMapper = userMapper;
     }
 
     public void register(UserRegisterDTO userRegisterDTO) {
 
+        userValidation(userRegisterDTO);
+
+        User user = this.userMapper.userDtoToUser(userRegisterDTO);
+
+        this.userRepository.save(user);
+
+    }
+
+    private void userValidation(UserRegisterDTO userRegisterDTO) {
         Optional<User> username = this.userRepository.findByUsername(userRegisterDTO.getUsername());
         Optional<User> email = this.userRepository.findByEmail(userRegisterDTO.getEmail());
 
@@ -32,12 +43,5 @@ public class AuthService {
         if (!userRegisterDTO.getConfirmPassword().equals(userRegisterDTO.getPassword())) {
             throw new RuntimeException("Passwords do not match!");
         }
-
-
-        User user = new User();
-
-        System.out.println(user);
-
-
     }
 }
